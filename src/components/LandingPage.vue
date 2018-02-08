0cef3ca35f674f3a5c0e2acecca0d5e72baa7feb
jeremy.clayton.91@gmail.com <jeremy.clayton.91@gmail.com>
2018-02-07 19:02:51 -0700

    configured styled build

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,13 +1,58 @@
+
 <template>
-  <h1>Landing Page</h1>
+<div id="testing">
+    <div class="block">
+      <span class="demonstration">Default</span>
+      <el-date-picker
+        v-model="value1"
+        type="date"
+        placeholder="Pick a day">
+      </el-date-picker>
+    </div>
+    <div class="block">
+      <span class="demonstration">Picker with quick options</span>
+      <el-date-picker
+        v-model="value2"
+        type="date"
+        placeholder="Pick a day"
+        :picker-options="pickerOptions1">
+      </el-date-picker>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'LandingPage'
-}
+  export default {
+    data() {
+      return {
+        pickerOptions1: {
+          disabledDate(time) {
+            return time.getTime() > Date.now();
+          },
+          shortcuts: [{
+            text: 'Today',
+            onClick(picker) {
+              picker.$emit('pick', new Date());
+            }
+          }, {
+            text: 'Yesterday',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit('pick', date);
+            }
+          }, {
+            text: 'A week ago',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', date);
+            }
+          }]
+        },
+        value1: '',
+        value2: '',
+      };
+    }
+  };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped style lang="scss">
-</style>
