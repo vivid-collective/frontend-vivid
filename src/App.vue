28914c8e64b43c9c99f6ae3e04b31aed9ade52a2
jeremy.clayton.91@gmail.com <jeremy.clayton.91@gmail.com>
2018-02-08 15:54:50 -0700

    brought in svgs

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
 <template>
   <div id="app">
-        <el-row>
-      <el-col :span="24">
-        <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal">
-          <el-menu-item index="1">Processing Center</el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">Workspace</template>
-            <el-menu-item index="2-1">item one</el-menu-item>
-          </el-submenu>
-          <el-menu-item index="3">Orders</el-menu-item>
-        </el-menu>
-      </el-col>
-    </el-row>
+    
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App"
-};
+  name: 'App'
+}
 </script>
 
 <style>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,58 +1,67 @@
 
 <template>
-<div id="testing">
-    <div class="block">
-      <span class="demonstration">Default</span>
-      <el-date-picker
-        v-model="value1"
-        type="date"
-        placeholder="Pick a day">
-      </el-date-picker>
-    </div>
-    <div class="block">
-      <span class="demonstration">Picker with quick options</span>
-      <el-date-picker
-        v-model="value2"
-        type="date"
-        placeholder="Pick a day"
-        :picker-options="pickerOptions1">
-      </el-date-picker>
-    </div>
+  <div id="testing">
+    <el-radio-group v-model="isCollapse">
+      <el-radio-button :label="false">expand</el-radio-button>
+      <el-radio-button :label="true">collapse</el-radio-button>
+    </el-radio-group>
+    <div class="nav-container">
+        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span slot="title">Navigator One</span>
+            </template>
+            <el-menu-item-group>
+              <span slot="title">Group One</span>
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <span slot="title">item four</span>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-setting"></i>
+            <span slot="title">Navigator Three</span>
+          </el-menu-item>
+        </el-menu>
+     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        pickerOptions1: {
-          disabledDate(time) {
-            return time.getTime() > Date.now();
-          },
-          shortcuts: [{
-            text: 'Today',
-            onClick(picker) {
-              picker.$emit('pick', new Date());
-            }
-          }, {
-            text: 'Yesterday',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24);
-              picker.$emit('pick', date);
-            }
-          }, {
-            text: 'A week ago',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit('pick', date);
-            }
-          }]
-        },
-        value1: '',
-        value2: '',
-      };
+export default {
+  data() {
+    return {
+      isCollapse: true
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
     }
-  };
+  }
+};
 </script>
+<style scoped style lang="scss">
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+el-radio-group {
+  display: flex !important;
+  justify-content: flex-end !important;
+}
+</style>
